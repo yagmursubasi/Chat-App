c4e482a9d7b9c3ede2318cbb42ae64089104e468
Yağmur Subaşı <yagmursubasi@Yagmur-Air.hgw.local>
2025-03-27 02:06:38 +0300

    update

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,21 @@
 import { auth } from "../firebase";
+import generateColor from "../utils/generateColor";
 
 const Message = ({ data }) => {
   //eğer mesajı oturumu açık olan kullanıcı açtıysa :sağ
   if (auth.currentUser.uid === data.author.id) {
-    return <div className="msg-user">{data.text}</div>;
+    return <p className="msg-user">{data.text}</p>;
   }
   //eğer mesajı farklı bir kullanıcı attıysa :sol
   return (
     <div className="msg-other">
+      <img src={data.author.photo} />
       <div>
-        <img src={data.author.photo} />
-        <span>{data.author.name} </span>
+        <span style={{ color: generateColor(data.author.id) }}>
+          {data.author.name}{" "}
+        </span>
+        <p className="msg-text">{data.text} </p>
       </div>
-      <p className="msg-text">{data.text} </p>
     </div>
   );
 };
